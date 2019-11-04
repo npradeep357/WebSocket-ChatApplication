6c3f12cb1f49a4199f6a9f6036c6b33b6b0f794f
Pradeep Neerukonda <pneeruko@opentext.com>
2019-11-04 19:40:11 +0530

    added jetty server impl and web socket impl

--- src/main/java/com/test/chat_ws/App.java
+++ src/main/java/com/test/chat_ws/App.java
@@ -1,13 +1,35 @@
 package com.test.chat_ws;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.test.chat_ws.exception.ServerException;
+import com.test.chat_ws.server.JettyServerImpl;
+
 /**
- * Hello world!
- *
+ * @author npradeep357
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    private static final Logger LOG = LoggerFactory.getLogger(App.class);
+
+    public static void main(
+            String[] args) {
+
+        // using auto closeable feature to stop server.
+        try (var server = new JettyServerImpl();) {
+
+            server.setPort(8080);
+
+            // Blocked call. Waits until server shutdown called either by using ctrl+c or shutdown URL request
+            server.start();
+
+        } catch (ServerException e) {
+            // for now just log it.
+            LOG.error(e.getMessage());
+        } catch (Exception e) {
+            LOG.error(e.getMessage());
+        }
+
     }
 }
