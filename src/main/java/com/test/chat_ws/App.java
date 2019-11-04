324d1c4e0631021b1bb0287fa825cabb7c1dc7fb
Pradeep Neerukonda <pneeruko@opentext.com>
2019-11-04 19:36:42 +0530

    Revert "updating code: Added Websocket impl and jetty server impl."
    This reverts commit fbc22570e2ed77f76375297208e8d006d299a431.

--- src/main/java/com/test/chat_ws/App.java
+++ src/main/java/com/test/chat_ws/App.java
@@ -1,35 +1,13 @@
 package com.test.chat_ws;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import com.test.chat_ws.exception.ServerException;
-import com.test.chat_ws.server.JettyServerImpl;
-
 /**
- * @author npradeep357
+ * Hello world!
+ *
  */
-public class App {
-
-    private static final Logger LOG = LoggerFactory.getLogger(App.class);
-
-    public static void main(
-            String[] args) {
-
-        // using auto closeable feature to stop server.
-        try (var server = new JettyServerImpl();) {
-
-            server.setPort(8080);
-
-            // Blocked call. Waits until server shutdown called either by using ctrl+c or shutdown URL request
-            server.start();
-
-        } catch (ServerException e) {
-            // for now just log it.
-            LOG.error(e.getMessage());
-        } catch (Exception e) {
-            LOG.error(e.getMessage());
-        }
-
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
     }
 }
