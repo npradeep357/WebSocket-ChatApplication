4c64af437062aa7cb5b119673b2d661a3b1e7f8f
Pradeep Neerukonda <pneeruko@opentext.com>
2019-11-04 14:03:29 +0530

    initial commmit

---
+++ src/main/java/com/test/chat_ws/App.java
@@ -0,0 +1,13 @@
+package com.test.chat_ws;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
